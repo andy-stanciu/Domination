537ff25642c6e63aa6c4c44bda15d199f356b084
Andy Huang <andyh23@lakesideschool.org>
2021-03-20 22:24:42 -0700

    AndySprint1

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerScript : MonoBehaviour
+{
+    private double wood;
+    private double food;
+    private double gold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      this.wood = 0.0;
+      this.food = 0.0;
+      this.gold = 0.0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Debug.Log(this.wood);
+    }
+
+    public void changeMaterial(string material, double number)
+    {
+      if (material == "wood")
+      {
+        this.wood += number;
+      }
+      else if (material == "food")
+      {
+        this.food += number;
+      }
+      else
+      {
+        this.gold += number;
+      }
+    }
+
+    public void setWood(int number)
+    {
+      this.wood = number;
+    }
+
+    public void setFood(int number)
+    {
+      this.food = number;
+    }
+
+    public void setGold(int number)
+    {
+      this.gold = number;
+    }
+
+    public int getWood()
+    {
+      return (int)Math.Floor(this.wood);
+    }
+
+    public int getFood()
+    {
+      return (int)Math.Floor(this.food);
+    }
+
+    public int getGold()
+    {
+      return (int)Math.Floor(this.gold);
+    }
+}
