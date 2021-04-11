c4870165500786184c17f6871ba3918f55acde95
yjb-alevo <officialkrythex@gmail.com>
2021-04-10 23:24:16 -0700

    Refactored scripts

--- Assets/Scripts/IHeapObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-
-public interface IHeapObject<T> : IComparable<T>
-{
-    int HeapIndex { get; set; }
-}
